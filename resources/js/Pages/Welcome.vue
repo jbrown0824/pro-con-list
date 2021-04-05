7801bfc8abb4a7484422e69e664e37b206f5aefa
Jeff <jbrown0824@gmail.com>
2021-04-05 09:45:09 -0700

    Fix homepage links

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,14 +1,9 @@
 <template>
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
         <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-			<template v-if="$page.props.user">
-				<inertia-link v-if="$page.props.user" href="/dashboard" class="text-sm text-gray-700 underline">
-					Dashboard
-				</inertia-link>
-				<inertia-link href="/lists/demo" class="text-sm text-gray-700 underline">
-					Demo List
-				</inertia-link>
-			</template>
+			<inertia-link v-if="$page.props.user" href="/dashboard" class="text-sm text-gray-700 underline">
+				Dashboard
+			</inertia-link>
 
             <template v-else>
                 <inertia-link :href="route('login')" class="text-sm text-gray-700 underline">
@@ -18,11 +13,11 @@
                 <inertia-link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 underline">
                     Register
                 </inertia-link>
-
-				<inertia-link href="/lists/demo" class="text-sm text-gray-700 underline">
-					Demo List
-				</inertia-link>
 			</template>
+
+			<inertia-link href="/lists/demo" class="ml-4 text-sm text-gray-700 underline">
+				Demo List
+			</inertia-link>
         </div>
 
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
